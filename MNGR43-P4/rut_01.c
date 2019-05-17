58275a4507c845c2a5ebe53d3b275ce7780a1e35
Marc-xyz <fermat.bohr@gmail.com>
2019-05-17 09:16:54 +0200

    modificat:        MNGR43-P4/rut_01.c

--- MNGR43-P4/rut_01.c
+++ MNGR43-P4/rut_01.c
@@ -25,7 +25,7 @@
 //Función que evalua la derivada del polinomio de Legendre enésimo para x.
  double d_polin_Legndr(double n,double x);
 //Funcion que calcula los coeficientes de P(x) de Legendre.
- double coefi_Legndr(double n,double roots[], unsigned int i);
+ double coefi_Legndr(double n,double ro);
 
 
 
@@ -64,9 +64,9 @@ int main(void)
       *         *Itervalos con almenos una raiz           *
       *         *Raices                                   *
       *****************************************************/
-     double points[TOL],
-             I_roots[2*(N)],
-	     roots[N];
+      double points[TOL+1],
+             I_roots[2*TOL],
+	         roots[N+1];
 
      /***********************Valores:***********************
       *          *Intervalo [a,b]=[-1,1]                   *
@@ -76,43 +76,45 @@ int main(void)
       *          *                                         *
       ******************************************************/
      double a=-1.0,b=1,
-	    x_i, x_ii,
+	    x_i=0, x_ii=0,
 	    o ,
 	    sum;
-     unsigned int j, i,  m=-1, I=-1; 
+    int j, i,  m=0, I=0; 
     //contador de raices, numero de intervalos por dos
     /*cabezera del progrma*/
    for(o=2;o<=N;o+=2)
    {
     sum=0.0;
-    /*Generamos intervalos*/   
-     for(j=0;j<=TOL;i++)
-     {points[1]=a+j*(b-a)/TOL;}
+    /*Generamos puntos intervalos*/   
+     for(j=0;j<=TOL;j++)
+     {points[j]=a+(j*(b-a)/TOL);}
      /*Buscamos intervalos con almenos una raiz*/
-     for(i=0;i<=TOL;i+=2)
+     for(i=1;i<TOL;i++)
  	 {
-		 if(polin_Legndr(o,points[i])*polin_Legendr(o,points[i+1])<0)
-		 {I+=2; I_roots[I-2]=points[i]; I_roots[I-1]=points[i]; }
+		 if(polin_Legndr(o,points[i-1])*polin_Legndr(o,points[i])<0)
+		 {I++; I_roots[I-1]=points[i-1];I++; I_roots[I-1]=points[i]; }
      		 if(points[i]*points[i+1]==0)
            	 {
-	         if(polin_Legendr(o,points[i])==0){m++;roots[m]=points[i];}
-         	 if(polin_Legendr(o,points[i+1])==0){m++;roots[m]=points[i+1];}
-                 }
+	         if(polin_Legndr(o,points[i-1])==0){m++;roots[m-1]=points[i];}
+         	 if(polin_Legndr(o,points[i])==0){m++;roots[m-1]=points[i-1];}
+             }
  	 }
+ 	 printf("Fins aqui");
      /*Comprovamos número intervalos*/
-     if((I+1)!=(2*o)){printf("\nERROR");}
+     if((I+1.0)!=(2.0*o)){printf("\nERROR");}
+     printf("Fins aqui");
      /*Aplicamos Newton para obtener raízes*/
-     for(j=0;j<=I;i+=2)
+     for(j=0;j<I;j+=2)
      {	
-	 x_i=(I_roots[j+1]-I_roots[j])/2.0;
-	 for(i=0;i<=1000;i++)
-          {x_ii=metNewd(o,x_i); if(fabs(x_i-x_ii)<=1.0e-7){m++;roots[m]=x_ii;} x_i=x_ii;}
+	 x_i=(I_roots[j+1]+I_roots[j])/2.0;
+	 for(i=0;i<=100;i++)
+          {x_ii=metNewd(o,x_i); if(fabs(x_i-x_ii)<=1.0e-7){m++;roots[m-1]=x_ii;} x_i=x_ii;}
      }
      /*Comprovamos número raices*/
      if((m+1)!=(o)){printf("\nERROR");}
      /*Calculamos suma final aproximada*/
-     for(j=1;j<=o;j++)
-     {sum+=coefi_Legndr(o,roots,i)*F(roots[j-1]);}
+     for(j=0;j<=o;j++)
+     {sum+=coefi_Legndr(o,roots[j-1])*F(roots[j-1]);}
      printf("\nNew  [%g], is %.16G",o,sum);
    }
     return 0;
@@ -128,9 +130,9 @@ int main(void)
  double polin_Legndr(double n,double x)
  {
      unsigned int i;
-     double P_i=1.0,P_ii=x,P_iii;
+     double P_i=1.0,P_ii=x,P_iii=0;
      if(n==0.0)
-    
+     {
          return P_i;
      }
      if(n==1.0)
@@ -153,16 +155,15 @@ double d_polin_Legndr(double n,double x)
 }
 
 //Funcion que calcula los coeficientes de P(x) de Legendre.
-  double coefi_Legndr(double n,double roots[], unsigned int i)
+  double coefi_Legndr(double n,double ro)
 {
-	double a= (2.0)/((1-roots[i-1])*(1-roots[i-1])\
-        *(d_polin_Legndr(n,roots[i-1])*d_polin_Legndr(n,roots[i-1])));
+	double a= (2.0)/((1-ro)*(1-ro)*(d_polin_Legndr(n,ro)*d_polin_Legndr(n,ro))) ;
 	return a;
 }
 //Funcion que implementan el algoritmo del Método de Newton en precisión doble.
 double metNewd(double n, double x)
 {
-    return x-(polin_Legndr(x))/(d_polin_Legndr(x));
+    return x-(polin_Legndr(n,x))/(d_polin_Legndr(n,x));
 }
 
 /*
